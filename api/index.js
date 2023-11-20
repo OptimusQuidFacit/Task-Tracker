04c7b824e0cb17183de6910b3846da67b645fd96
OptimusQuidFacit <chimkaemewiseman@gmail.com>
2023-11-20 14:34:58 +0100

    build added

--- api/index.js
+++ api/index.js
@@ -5,14 +5,22 @@ const mongoose= require('mongoose');
 dotenv.config();
 const taskRouter=require('./routers/tasks');
 const cors= require('cors');
+const path = require('path');
 
 
-mongoose.connect(process.env.MONGO_SEC).then(()=>{
+mongoose.connect(process.env.MONGO_SEC)
+.then(()=>{
     console.log("Connection Successful");
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err);
 })
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(cors());
 app.use(express.json())
